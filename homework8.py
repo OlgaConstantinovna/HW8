0a4fc5a2fba768ac1f23194d3eb40f16b5544ee8
Olga <princessca.olga@icloud.com>
2023-02-05 16:15:19 +0300

    all

--- homework8.py
+++ homework8.py
@@ -29,5 +29,71 @@ def zadacha1():
             
 
     print(f'максимальный средний бал у группы {group_max}')  
-    print(f' максимальный бал рвен {mark_max}')          
+    print(f' максимальный бал рвен {mark_max}')   
+
+# Задача 2. Дана квадратная матрица, заполненная случайными числами. 
+# Определите, сумма элементов каких строк превосходит сумму главной диагонали матрицы.
+def zadacha2():
+
+    rows = 4
+    numbers =[0]* rows
+
+    for i in range(rows):
+        numbers[i] = [random.randint(1, 100) for _ in range(rows)]
+
+    for row in numbers:
+        print(row)    
+
+    diagonal_sum = 0
+    for i in range(rows):
+        diagonal_sum += numbers[i][i]
+    # print(f'сумма элементов главной диагонали равна { diagonal_sum}')
+
+    for index in range(len(numbers)):
+        sum_in_row = sum(numbers[index])
+        if sum_in_row > diagonal_sum:
+            print(f'в строке {index+1}, сумма элементов {numbers[index]} = {sum_in_row} превышает сумму элементов ГД {diagonal_sum}')   
+
+# Задача 3. В двумерном массиве хранятся средние дневные температуры с мая по сентябрь за прошлый год. Каждому месяцу соответствует своя строка.
+# Определите самый жаркий и самый холодный 7-дневный промежуток каждого месяца. Выведите их даты.
+def period_for_mounth(temperature_in_mounth, mounth):
+    max_temp = 0
+    day_max_temp = 1
+    min_temp = 1000
+    day_min_temp = 1
+    period = 7
+    for day in range(len(temperature_in_mounth) - period + 1):
+        temp_in_period = temperature_in_mounth[day:day + period]
+        sum_temp_in_period = sum(temp_in_period)
+        # print(f'{day + 1} - {day + period} {temp_in_period}')
+        if sum_temp_in_period >  max_temp:
+            max_temp = sum_temp_in_period
+            day_max_temp = day
+        elif sum_temp_in_period < min_temp:
+            min_temp = sum_temp_in_period
+            day_min_temp = day
+    print(f'максимальная температура равна {round(max_temp/period,1)} была c {day_max_temp+1} по {day_max_temp + period} {mounth}')
+    print(f'минимальная температура равна {round(min_temp/period,1)} была с {day_min_temp + 1} по {day_min_temp + period} {mounth}')   
+
+def zadazha3():
+
+    temp_in_may = [random.randint(12, 23) for _ in range(31)]
+    temp_in_june =[random.randint(16,28) for _ in range(30)]
+    temp_in_july =[random.randint(19,32) for _ in range(31)]
+    temp_in_august =[random.randint(20,28) for _ in range(30)]
+    temp_in_september =[random.randint(15,24) for _ in range(31)]
+    print(f'все температуры {temp_in_may}')
+    period_for_mounth(temp_in_may, 'мая')
+    print(f'все температуры {temp_in_june}')
+    period_for_mounth(temp_in_june, 'июня')
+    print(f'все температуры {temp_in_july}')
+    period_for_mounth(temp_in_july, 'июля')
+    print(f'все температуры {temp_in_august}')
+    period_for_mounth(temp_in_august, 'августа')
+    print(f'все температуры {temp_in_september}')
+    period_for_mounth(temp_in_september, 'сентября')
+   
+
+zadazha3()
+# zadacha2()
 # zadacha1()
